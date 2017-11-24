9c6d0448aeb7eec2d90ab5be1a77fa35d6b09b27
gabee1987 <gabee1987@gmail.com>
2017-11-24 15:17:34 +0100

    Threading - Exercise 2 and 3 is done

---
+++ SingleInstance/SingleInstanceExercise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleInstance
+{
+    class SingleInstanceExercise
+    {
+        static void Main(string[] args)
+        {
+            const string mutexName = "RUNMEONLYONCE";
+            Mutex mutex = null;
+
+            while (true)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(mutexName);
+                    Console.WriteLine("Mutex found, exiting..");
+                    mutex.Close();
+                    break;
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    bool mutexIsMine = true;
+                    mutex = new Mutex(mutexIsMine, mutexName);
+                    Console.WriteLine("Mutex not found, created.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadPoolDemo/ThreadPoolDemoExercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    class ThreadPoolDemoExercise
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.ReadKey();
+        }
+
+        static public void ShowMyText(object state)
+        {
+            string txt = "State: " + (string)state;
+            txt += " Thread: " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(txt);
+        }
+    }
+}
